b31b13c04fb9f1b9f7b103ab5e51a0e671a230a5
BigETI <bigeti.ethem@gmail.com>
2022-09-07 22:42:53 +0200

    Implemented list comparators.

---
+++ Taurus/Comparators/Static/ListComparators.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Taurus comparators namespace
+/// </summary>
+namespace Taurus.Comparators
+{
+    /// <summary>
+    /// A class that contains functionalities to compare lists
+    /// </summary>
+    public static class ListComparators
+    {
+        /// <summary>
+        /// Compares the specfied lists with each other
+        /// </summary>
+        /// <typeparam name="TValue">Value type</typeparam>
+        /// <param name="leftList">Left list</param>
+        /// <param name="rightList">Right list</param>
+        /// <returns>"true" if the specified lists are equivalent, otherwise "false"</returns>
+        public static bool CompareLists<TValue>(IReadOnlyList<TValue> leftList, IReadOnlyList<TValue> rightList)
+        {
+            bool ret = leftList == rightList;
+            if (!ret && (leftList.Count == rightList.Count))
+            {
+                ret = true;
+                for (int index = 0; index < leftList.Count; index++)
+                {
+                    TValue left_value = leftList[index];
+                    TValue right_value = rightList[index];
+                    if (!Equals(left_value, right_value))
+                    {
+                        ret = false;
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
